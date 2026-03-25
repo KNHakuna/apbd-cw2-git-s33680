2799a7e6bf724db172ab1043ffe5c469ebc56d21
Konrad Niedziółka <s33680@pjwstk.edu.pl>
2026-03-25 17:54:01 +0100

    Repair project structure and added Equipment and status enum

---
+++ cw2/Equipments/Equipment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Equipment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public EquipmentStatus Status { get; set; }
+
+    private static int nextId = 1;
+    public Equipment(string name)
+    {
+        Id = nextId;
+        nextId++;
+        Name = name;
+        Status = EquipmentStatus.Available;
+    }
+}
---
+++ cw2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var test1 =  new Equipment("Test1");
+        var test2 =  new Equipment("Test2");
+        Console.WriteLine(test1.Id+" "+test1.Name+" "+test1.Status);
+        Console.WriteLine(test2.Id+" "+test2.Name+" "+test2.Status);
+    }
+}
